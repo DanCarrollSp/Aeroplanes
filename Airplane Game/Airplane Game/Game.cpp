d10f9f6f391e21e17befc4ddcd010fcafe05bca3
(Student) - Daniel Carroll <c00272187@itcarlow.ie>
2022-02-02 15:31:02 +0000

    Added SFML template with dev info

---
+++ Airplane Game/Airplane Game/Game.cpp
@@ -0,0 +1,141 @@
+//Name: Daniel Carroll
+//Login: C00272187
+//Date: 2/2/22
+//Program: Airplane game
+//Time taken: N/A
+//Known bugs
+// None
+
+
+#include "Game.h"
+#include <iostream>
+
+
+/// default constructor
+/// setup the window properties
+/// load and setup the text 
+/// load and setup thne image
+Game::Game() :
+	m_window{ sf::VideoMode{ 800U, 600U, 32U }, "SFML Game" },
+	m_exitGame{false} //when true game will exit
+{
+	setupFontAndText(); // load font 
+	setupSprite(); // load texture
+}
+
+
+/// default destructor we didn't dynamically allocate anything
+/// so we don't need to free it, but mthod needs to be here
+Game::~Game()
+{
+}
+
+
+/// main game loop
+/// update 60 times per second,
+/// process update as often as possible and at least 60 times per second
+/// draw as often as possible but only updates are on time
+/// if updates run slow then don't render frames
+void Game::run()
+{	
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	const float fps{ 60.0f };
+	sf::Time timePerFrame = sf::seconds(1.0f / fps); // 60 fps
+	while (m_window.isOpen())
+	{
+		processEvents(); // as many as possible
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > timePerFrame)
+		{
+			timeSinceLastUpdate -= timePerFrame;
+			processEvents(); // at least 60 fps
+			update(timePerFrame); //60 fps
+		}
+		render(); // as many as possible
+	}
+}
+
+
+/// handle user and system events/ input
+/// get key presses/ mouse moves etc. from OS
+/// and user :: Don't do game update here
+void Game::processEvents()
+{
+	sf::Event newEvent;
+	while (m_window.pollEvent(newEvent))
+	{
+		if ( sf::Event::Closed == newEvent.type) // window message
+		{
+			m_exitGame = true;
+		}
+		if (sf::Event::KeyPressed == newEvent.type) //user pressed a key
+		{
+			processKeys(newEvent);
+		}
+	}
+}
+
+
+/// deal with key presses from the user
+/// <param name="t_event">key press event</param>
+void Game::processKeys(sf::Event t_event)
+{
+	if (sf::Keyboard::Escape == t_event.key.code)
+	{
+		m_exitGame = true;
+	}
+}
+
+
+/// Update the game world
+/// <param name="t_deltaTime">time interval per frame</param>
+void Game::update(sf::Time t_deltaTime)
+{
+	if (m_exitGame)
+	{
+		m_window.close();
+	}
+}
+
+
+/// draw the frame and then switch buffers
+void Game::render()
+{
+	m_window.clear(sf::Color::White);
+	m_window.draw(m_welcomeMessage);
+	m_window.draw(m_logoSprite);
+	m_window.display();
+}
+
+
+/// load the font and setup the text message for screen
+void Game::setupFontAndText()
+{
+	if (!m_ArialBlackfont.loadFromFile("ASSETS\\FONTS\\ariblk.ttf"))
+	{
+		std::cout << "problem loading arial black font" << std::endl;
+	}
+	m_welcomeMessage.setFont(m_ArialBlackfont);
+	m_welcomeMessage.setString("SFML Game");
+	m_welcomeMessage.setStyle(sf::Text::Underlined | sf::Text::Italic | sf::Text::Bold);
+	m_welcomeMessage.setPosition(40.0f, 40.0f);
+	m_welcomeMessage.setCharacterSize(80U);
+	m_welcomeMessage.setOutlineColor(sf::Color::Red);
+	m_welcomeMessage.setFillColor(sf::Color::Black);
+	m_welcomeMessage.setOutlineThickness(3.0f);
+
+}
+
+
+/// load the texture and setup the sprite for the logo
+void Game::setupSprite()
+{
+	if (!m_logoTexture.loadFromFile("ASSETS\\IMAGES\\SFML-LOGO.png"))
+	{
+		// simple error message if previous call fails
+		std::cout << "problem loading logo" << std::endl;
+	}
+	m_logoSprite.setTexture(m_logoTexture);
+	m_logoSprite.setPosition(300.0f, 180.0f);
+}
